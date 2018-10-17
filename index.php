874df2c8aed1da1bcd24330c7c4c04cf5029b6dd
teerapat <teerapat-yut.com>
2018-10-17 20:55:54 +0700

    start

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Title of the document</title>
+<link rel="stylesheet" type="text/css" href="assets/bootstrap/bootstrap.min.css">
+<link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="assets/custom/customIndex.css">
+<link rel="stylesheet" type="text/css" href="assets/custom/customIndexMedia.css">
+
+</head>
+
+<body>
+
+<!-- <div class="container-fluid"> -->
+<!-- 	<img class="d-block w-100 v_desktop" src="assets/image/01.png" alt="First slide">
+	<img class="d-block w-100 v_mobile" src="assets/image/01_mobile.png" alt="First slide">
+	<div class="row positionWellcome">
+		<div class="col-lg-8 col-lg-offset-1">
+			<div class="row">
+				<div class="txt_wellcome p-3">
+					<strong class="h1 text-shadow">MemberIn</strong><br>
+					เป็น Platform ใหม่ที่รวมเเหล่งกิจกรรมดี
+					ที่จะทำให้คุณได้ 
+					<strong class="h3 text-red text-shadow">กิน</strong>
+					<strong class="h3 text-green text-shadow">เที่ยว</strong>
+					<strong class="h3 text-yellow text-shadow">ช้อป</strong>
+					ได้อย่่างสะดวกสบายเเละทราบสิทธิพิเศษได้ทุกที่ทุกเวลา
+				</div>
+			</div>
+		</div>
+	</div> -->
+<!-- </div> -->
+
+<script src="assets/jquery/jquery.min.js"></script>
+<script src="assets/bootstrap/bootstrap.min.js"></script>
+<script src="assets/custom/customIndex_controller.js"></script>
+
+</body>
+	
+</html>
