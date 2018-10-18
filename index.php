ded38646b7de74d2acfba52793a6d3c8da70efd1
NotebookHP <teerapat-yut@hotmail.com>
2018-10-18 17:21:06 +0700

    respositive

--- index.php
+++ index.php
@@ -2,62 +2,157 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+<meta name="viewport" content="width=device-width,
+ height=device-height, initial-scale=1.0, minimum-scale=1.0">
 <title>Title of the document</title>
 <link rel="stylesheet" type="text/css" href="assets/bootstrap/bootstrap.min.css">
 <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="assets/custom/customIndex.css">
-<link rel="stylesheet" type="text/css" href="assets/custom/customIndexMedia.css">
+<link rel="stylesheet" type="text/css" href="assets/custom/customIndexMedia_sm.css">
+<link rel="stylesheet" type="text/css" href="assets/custom/customIndexMedia_lg.css">
 
 </head>
 
 <body>
 
 <!-- <div class="container-fluid"> -->
-<!-- 	<img class="d-block w-100 v_desktop" src="assets/image/01.png" alt="First slide">
-	<img class="d-block w-100 v_mobile" src="assets/image/01_mobile.png" alt="First slide">
-	<div class="row positionWellcome">
-		<div class="col-lg-8 col-lg-offset-1">
-			<div class="row">
-				<div class="txt_wellcome p-3">
-					<strong class="h1 text-shadow">MemberIn</strong><br>
-					เป็น Platform ใหม่ที่รวมเเหล่งกิจกรรมดี
-					ที่จะทำให้คุณได้
-					<strong class="h3 text-red text-shadow">กิน</strong>
-					<strong class="h3 text-green text-shadow">เที่ยว</strong>
-					<strong class="h3 text-yellow text-shadow">ช้อป</strong>
-					ได้อย่่างสะดวกสบายเเละทราบสิทธิพิเศษได้ทุกที่ทุกเวลา
+	<!-- 	<img class="d-block w-100 v_desktop" src="assets/image/01.png" alt="First slide">
+		<img class="d-block w-100 v_mobile" src="assets/image/01_mobile.png" alt="First slide">
+		<div class="row positionWellcome">
+			<div class="col-lg-8 col-lg-offset-1">
+				<div class="row">
+					<div class="txt_wellcome p-3">
+						<strong class="h1 text-shadow">MemberIn</strong><br>
+						เป็น Platform ใหม่ที่รวมเเหล่งกิจกรรมดี
+						ที่จะทำให้คุณได้ 
+						<strong class="h3 text-red text-shadow">กิน</strong>
+						<strong class="h3 text-green text-shadow">เที่ยว</strong>
+						<strong class="h3 text-yellow text-shadow">ช้อป</strong>
+						ได้อย่่างสะดวกสบายเเละทราบสิทธิพิเศษได้ทุกที่ทุกเวลา
+					</div>
 				</div>
+			</div>
+		</div> -->
+<!-- </div> -->
+<div class="content2">
+	<div class="row">
+		<!-- <img class="bg" src="assets/image/bg (1).png"> -->
+		<h2>ประสบการณ์ดี ๆ จากลูกค้าที่เคยใช้ <strong class="text-green text-shadow">MemberIn</strong></h2>
+		<div class="col-lg-6 col-lg-offset-3 ">
+			<div class="embed-responsive embed-responsive-16by9 bg-shadow">
+				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ibavfrxEPs8?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+			</div>
+			<!-- <img class="borderYoutube" src="assets/image/borderYoutube.png"> -->
+
+		</div>
+		<div class="peploe col-lg-4 col-lg-offset-7">
+			<img class="" src="assets/image/peploe.png">
+		</div>
+		
+	</div>
+</div>
+
+<div class="content3 bg-green">
+	<div class="cartoonDog">
+		<img src="assets/image/cartoonDog.png">
+		<div>ทำไมคุณต้องใช้ <br><strong class="text-red text-shadow">MemberIn</strong> ?</div>
+	</div>
+	<div class="content3_head_view_sm ">
+		ทำไมคุณต้องใช้  <strong class="text-red text-shadow">MemberIn</strong> ?
+	</div>
+	<div class="row">
+		<div class="col-lg-6">
+			<div class="content3_barnd">
+				<img src="assets/image/icon (1).png"><br>
+				<div class="input1 bg-yellow">ไม่พลาดสิทธิพิเศษ</div>
+				<div class="input2">โปรโมชั่นเด็ดๆที่คุณสนใจอยู่</div>
 			</div>
 		</div>
-	</div> -->
-<!-- </div> -->
-
-<div class="backgroundYoutube">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-5 col-xl-offset-1">
-				<div class="embed-responsive embed-responsive-16by9">
-					<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ibavfrxEPs8?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-
-				</div>
-
+		<div class="col-lg-6">
+			<div class="content3_barnd">
+				<img src="assets/image/icon (4).png"><br>
+				<div class="input1 bg-yellow">ไม่ต้องโหลดหลายเอพฯ</div>
+				<div class="input2">ให้รกหน้าจอ</div>
 			</div>
-			<div class="col-lg-5">
-				<img class="d-block w-100 v_mobile" src="assets/image/pepole.png" alt="First slide">
+		</div>
+		<div class="col-lg-6">
+			<div class="content3_barnd">
+			<img src="assets/image/icon (2).png"><br>
+			<div class="input1 bg-yellow">ใช้งานง่ายได้ทุกที่ทุกเวลา</div>
+			<div class="input2">เพียงเเค่กดโชว์/เเสกน</div>
+		</div>
+		</div>
+		<div class="col-lg-6">
+			<div class="content3_barnd">
+				<img src="assets/image/icon (5).png"><br>
+				<div class="input1 bg-yellow">ไม่ต้องพกหลายบัตร</div>
+				<div class="input2">ให้หนักกระเป๋า</div>
 			</div>
-
-
+		</div>
+		<div class="col-lg-6">
+			<div class="content3_barnd">
+				<img src="assets/image/icon (3).png"><br>
+				<div class="input1 bg-yellow">ชำระง่าย และ ปลอดภัย</div>
+				<div class="input2">มันใจด้วยระบบ Verify OTP</div>
+			</div>
+		</div>
+		<div class="col-lg-6">
+			<div class="content3_barnd">
+				<img src="assets/image/icon (6).png"><br>
+				<div class="input1 bg-yellow">ยิ่งใช้มากยิ่งสะสมเเต้ม</div>
+				<div class="input2">เพื่อนำมาเเลกสิทธิพิเศษ/ส่วนลดต่าง ๆ</div>
+			</div>
 		</div>
 	</div>
 </div>
 
-
+<div class="content4 text-green">
+	<div class="row">
+		<h1>วิธีลงทะเบียน</h1>
+		<div class="col-lg-5 col-lg-offset-5">
+			<div class="num">
+				<img src="assets/image/button (1).png">
+			</div>
+			<div class="text">
+				ดาวโหลด MemberIn ผ่าน 
+				<img src="assets/image/appleStore.png">
+				หรือ
+				<img src="assets/image/googlePlay.png">
+			</div>
+		</div>
+		<div class="col-lg-5 col-lg-offset-5">
+			<div class="num">
+				<img src="assets/image/button (2).png">
+			</div>
+			<div class="text">
+				กรอกข้อมูลเพื่อลงทะเบียน
+			</div>
+		</div>
+		<div class="col-lg-5 col-lg-offset-5">
+			<div class="num">
+				<img src="assets/image/button (3).png">
+			</div>
+			<div class="text">
+				ยืนยันตัวตนผ่านเบอร์โทรศัพท์มือถือ หรือ อีเมล
+			</div>
+		</div>
+		<div class="col-lg-5 col-lg-offset-5">
+			<div class="num">
+				<img src="assets/image/button (4).png">
+			</div>
+			<div class="text">
+				เพลิดเพลินกับสิทธฺต่างๆ มากมายไปกับ MemberIn
+			</div>
+		</div>
+	</div>
+	
+</div>
 
 <script src="assets/jquery/jquery.min.js"></script>
 <script src="assets/bootstrap/bootstrap.min.js"></script>
 <script src="assets/custom/customIndex_controller.js"></script>
 
 </body>
-
+	
 </html>
