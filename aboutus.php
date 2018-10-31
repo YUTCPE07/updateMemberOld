bfd62462095141bf3bfd4c11f7b3f17d4151f4fd
NotebookHP <teerapat-yut@hotmail.com>
2018-10-31 11:24:02 +0700

    aboutus sucess

--- aboutus.php
+++ aboutus.php
@@ -6,18 +6,108 @@
 <meta name="viewport" content="width=device-width,
  height=device-height, initial-scale=1.0, minimum-scale=1.0">
 <title>.:: MemberIn ::.</title>
-<link rel="stylesheet" type="text/css" href="assets/bootstrap/bootstrap.min.css">
-<link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+<!-- <link rel="stylesheet" type="text/css" href="assets/bootstrap/bootstrap.min.css"> -->
 
-<link rel="stylesheet" type="text/css" href="assets/custom/customIndex.css">
-<link rel="stylesheet" type="text/css" href="assets/custom/customIndexMedia_sm.css">
-<link rel="stylesheet" type="text/css" href="assets/custom/customIndexMedia_lg.css">
-<link rel="stylesheet" type="text/css" href="assets/custom/customIndexMedia_xl.css">
+
 
 </head>
 
 <body>
+	<link rel="stylesheet" type="text/css" href="assets/custom/reset.css">
+	<link href="https://fonts.googleapis.com/css?family=Prompt:100,300,400,500,700&amp;subset=thai" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/custom/varible.css">
+	<link rel="stylesheet" type="text/css" href="assets/css_page/aboutus.css">
+
+
+	<div class="content1 text-justify pt-5 ">
+		<div class="container">
+			<div class="h2 medium text-black pb-3">เกี่ยวกับเรา</div>
+			<div class="d-inline h4 medium text-green">MemberIn</div>
+			<div class="d-inline h4 light">เป็น Plateform ที่รวมแหล่งกิจกรรมดี ๆ ที่ทำให้คุณ</div>
+			<div class="d-inline h4 medium text-red">กิน</div>
+			<div class="d-inline h4 medium text-green">เที่ยว</div>
+			<div class="d-inline h4 medium text-yellow">ช้อป</div>
+			<div class="d-inline h4 light">
+				ได้อย่างสะดวกสบาย และทราบข่าวสารสิทธิพิเศษ ได้ทุกที่ ทุกเวลา บนระบบที่มีมาตรฐานความปลอดภัย https ระดับสากล แบบเดียวกับ Lazada , Agoda และอื่น ๆ มีระบบการจ่ายชำระมาตรฐานเดียวกับระบบธนาคาร
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="content2 h4 text-black pt-3">กิจกรรมดี ๆ  ในหลากหลายหมวดหมู่ ที่มีให้คุณเลือก</div>
+	</div>
+
+	<div class="content3 pt-3 light ">
+		<div class="container">
+			<div class="h4">•	ผจญภัย </div>
+			<div class="h4">•	ความงาม และ สุขภาพ </div>
+			<div class="h4">•	ครอส์เรียนระยะสั้น </div>
+			<div class="h4">•	กิจกรรมบันเทิง </div>
+			<div class="h4">•	อาหาร </div>
+			<div class="h4">•	เกม </div>
+			<div class="h4">•	กิจกรรมกลางแจ้ง </div>
+			<div class="h4">•	ช็อปปี้ง </div>
+			<div class="h4">•	การแสดง </div>
+			<div class="h4">•	ทัวร์ & ล่องเรือ </div>
+			<div class="h4">•	สวนสนุก </div>
+			<div class="h4">•	อื่น ๆ </div>
+		</div>
+	</div>
+
+	<div class="content4 pt-3 light">
+		<div class="container">
+			<div class="h4 d-inline medium text-green pt-3">MemberIn</div>
+			<div class="h4 d-inline">เสนอกิจกรรมดี ๆ ผ่าน 2 ช่องทาง</div>
+			<div class="h4 d-inline text-black">Website</div>
+			<div class="h4 d-inline">,</div>
+			<div class="h4 d-inline text-black">Mobile Application</div>
+			<div class="h4 d-inline">ให้แก่ ลูกค้าด้วย 3 วิธีใช้ แบบง่าย ๆ</div>
+
+			<div class="h4">1.	SHOPPING ONLINE  เรามอบสิทธิพิเศษในการซื้อสินค้า และการบริการที่หลากหลายได้ง่ายๆ เพียงปลายนิ้ว</div>
+			<div class="h4">2.	GET PROMOTION   แหล่งรวบรวมข่าวสาร โปรโมชั่น จากหลายแบรนด์ และหลายร้านค้า พร้อมกดรับสิทธิ์ ได้</div>
+			<div class="h4">3.	REGISTER MEMBER. รวบรวมข้อมูลสิทธิประโยชน์ของสมาชิก พร้อมให้คุณสามารถสมัครสมาชิกออนไลน์ได้ทันที</div>
+		</div>
+	</div>
+
+
+	<div class="content5 pt-5 pb-5 ">
+		<div class="h4 text-black medium pb-5">Experience ประสบการณ์ดีๆ จากลูกค้าที่เคยใช้ MemberIn </div>
+		<div class="content5-video">
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe class="embed-responsive-16by9" src="https://www.youtube.com/embed/ibavfrxEPs8?rel=0&amp;controls=1&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+			</div>
+		</div>
+	</div>
+
+	<div class="content6 light text-gray1">
+		<div class="container">
+			<div class="h4 text-black medium">ทำไมคุณต้องใช้ MemberIn</div>
+			<div class="h4">1.	ไม่พลาดกิจกรรมดี ๆ  โปรโมชั่นเด็ดๆ ที่คุณสนใจอยู่</div>
+			<div class="h4">2.	ใช้งานได้ทุกที่ทุกเวลาเพียงแค่กด โชว์ หรือ สแกน</div>
+			<div class="h4">3.	ง่ายและปลอดภัยในการชำระเงินออนไลน์</div>
+			<div class="h4">4.	ยิ่งใช้มาก ยิ่งสะสมแต้มได้มาก เพื่อใช้ แลกของรางวัล ดี ๆ ที่รอคุณอยู่</div>
+		</div>
+	</div>
+
+
+	<div class="content7 light text-gray1 pt-5">
+		<div class="container">
+			<div class="h4 text-black medium">เริ่มได้เลยเพียง</div>
+			<div class="h4 d-inline">1.	เข้า www.memberin.com หรือ ดาวน์โหลด MembeIn ผ่าน</div>
+				<img class="content7 appleStore d-inline this_link" src="assets/image/appleStore.png">
+				<div class="content7 regular d-inline">หรือ</div>
+				<img class="content7 googlePlay this_link" src="assets/image/googlePlay.png">
+			<div class="h4">2.	กรอกข้อมูลเพื่อลงทะเบียน</div>
+			<div class="h4">3.	ยืนยันตัวตนผ่าน เบอร์โทรศัพท์ หรือ อีเมล์</div>
+			<div class="h4">4.	เพลิดเพลินกับสิทธิ์ต่างๆ มากมาย ไปกับ MemberIn</div>
+		</div>
+	</div>
+
+	<div class="content8">
+		<div class="container">
+			<div class="h4 text-black medium pt-3 pb-3">เตรียมพบกับกิจกรรมต่าง ๆ ในมือคุณได้แล้ว อย่ารอช้า</div><br><br>
+		</div>
+	</div>
 
 </body>
 
--- termsofuse.php
+++ termsofuse.php
@@ -6,14 +6,15 @@
 <meta name="viewport" content="width=device-width,
  height=device-height, initial-scale=1.0, minimum-scale=1.0">
 <title>.:: MemberIn ::.</title>
-<link rel="stylesheet" type="text/css" href="assets/bootstrap/bootstrap.min.css">
-<link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+<!-- <link rel="stylesheet" type="text/css" href="assets/bootstrap/bootstrap.min.css"> -->
 
-<link rel="stylesheet" type="text/css" href="assets/custom/customIndex.css">
-<link rel="stylesheet" type="text/css" href="assets/custom/customIndexMedia_sm.css">
-<link rel="stylesheet" type="text/css" href="assets/custom/customIndexMedia_lg.css">
-<link rel="stylesheet" type="text/css" href="assets/custom/customIndexMedia_xl.css">
+<link href="https://fonts.googleapis.com/css?family=Prompt:100,300,400,500,700&amp;subset=thai" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="assets/custom/reset.css">
+<link rel="stylesheet" type="text/css" href="assets/custom/varible.css">
+
+<link rel="stylesheet" type="text/css" href="assets/custom/footer.css">
+<link rel="stylesheet" type="text/css" href="assets/custom/main.css">
+<link rel="stylesheet" type="text/css" href="assets/custom/media_sm.css">
 
 </head>
 
